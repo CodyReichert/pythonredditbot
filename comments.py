50257734760cbe41e6cf521e78498c4d0fcb4c3d
CodyReichert <cody@reichertbrothers.com>
2014-07-05 12:27:28 -0500

    most funcationality working

--- comments.py
+++ comments.py
@@ -14,68 +14,66 @@ USERAGENT = "Hitler counter by /u/SirCaptain"
 #This is a short description of what the bot does. For example "/u/GoldenSights' Newsletter bot"
 SUBREDDIT = "all"
 #This is the sub or list of subs to scan for new posts. For a single sub, use "sub1". For multiple subreddits, use "sub1+sub2+sub3+..."
-PARENTSTRING = ["the"]
+PARENTSTRING = ["the", "hitler", "Hitler", "HITLER"]
 #These are the words you are looking for
-REPLYSTRING = "Way to go, we went a whole 5 minutes without mentioning Hitler"
+REPLYSTRING = "Way to go, Reddit was Hitler free for "
 #This is the word you want to put in reply
 MAXPOSTS = 100
 #This is how many posts you want to retrieve all at once. PRAW can download 100 at a time.
 WAIT = 20
 #This is how many seconds you will wait between cycles. The bot is completely inactive during this time.
 
-
 '''All done!'''
 
-
-
-
 WAITS = str(WAIT)
-try:
-    import bot #This is a file in my python library which contains my Bot's username and password. I can push code to Git without showing credentials
-    USERNAME = bot.getu()
-    PASSWORD = bot.getp()
-    USERAGENT = bot.geta()
-except ImportError:
-    pass
 
+# Connect to DB, create if its not there
 sql = sqlite3.connect('sql.db')
 print('Loaded SQL Database')
 cur = sql.cursor()
 
-cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT, timestamp TEXT)')
+#create table to posts we've replied to
+cur.execute('CREATE TABLE IF NOT EXISTS oldposts(ID TEXT, timestamp DATE, permalink TEXT)')
 print('Loaded Completed table')
 
 sql.commit()
 
+# log the bot into reddit
 r = praw.Reddit(USERAGENT)
 r.login(USERNAME, PASSWORD) 
 
+# Main loo[
 def scanSub():
     print('Searching '+ SUBREDDIT + '.')
     subreddit = r.get_subreddit(SUBREDDIT)
     posts = subreddit.get_comments(limit=MAXPOSTS)
-    print(posts)
+    # Get the latest timestamp in our database for comparison
+    lastrecent = cur.execute("SELECT * FROM oldposts WHERE timestamp = (SELECT MAX(timestamp) from oldposts)")
+    lastrecent = float(cur.fetchone()[1])
+    lastpermalink = cur.fetchone()[2]
     for post in posts:
         pid = post.id
-        pts = str(post.created)
-        dt1 = datetime.datetime.fromtimestamp(post.created)
-        readable = dt1.strftime('%Y-%m-%d %H:%M:%S')
+        pts = post.created
+        permalink = post.permalink
         try:
             pauthor = post.author.name
         except AttributeError:
             pauthor = '[DELETED]'
         cur.execute('SELECT * FROM oldposts WHERE ID=?', [pid])
         if not cur.fetchone():
-            cur.execute('INSERT INTO oldposts VALUES(?, ?)', [pid, pts])
+            cur.execute('INSERT INTO oldposts VALUES(?, ?, ?)', [pid, pts, permalink])
+            # Get the time since last comment about Hitler
+            current_timestamp = datetime.datetime.fromtimestamp(pts)
+            first_timestamp = datetime.datetime.fromtimestamp(lastrecent)
+            tl = dateutil.relativedelta.relativedelta(current_timestamp, first_timestamp)
+            ##############################################
             pbody = post.body.lower()
             if any(key.lower() in pbody for key in PARENTSTRING):
                 if pauthor != USERNAME:
-                    print('Replying to ' + pid + ' by ' + pauthor + ' at ' + readable)
-                else:
-                    print('Will not reply to self')
-    sql.commit()
+                    print('Replying to ' + pid + ' by ' + pauthor + ' at ' + str(pts))
+                    print(REPLYSTRING + '%d hours %d minutes %d seconds' % (tl.hours, tl.minutes, tl.seconds))
 
-
+# Run loop, wait, run loop
 while True:
     try:
         scanSub()
